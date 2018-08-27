aa014e096d7d74679a8dafdadde3a60d05e11350
Voxoff <guybennettjones@hotmail.co.uk>
2018-08-27 14:51:09 +0100

    reviews

--- scrape.rb
+++ scrape.rb
@@ -15,24 +15,23 @@
 # html_doc.search('.feedback-content .ml-1') do |element|
 #   puts element.text.strip
 # end
-class Scrape
-  def method
-
-  end
-end
 hash = {}
 
 (1..6).each do |element|
   element = "" if element == 1
-  file = File.read("Kitt#{element}.html")
+  file = File.read("../../lewagon/reviews/Kitt#{element}.html")
   html_doc = Nokogiri::HTML(file)
   html_doc.search('.student-content').each do |e|
     img =  e.search('.img-md-square').to_a.first.attributes["src"].value
     name = e.search('.feedback-content').children.first.text.strip
     review = e.search('.feedback-content').children.search('.ml-1').text.strip
-    hash[name] = [img,review] unless review.empty?
+    hash[name] = {img: img, review: review} unless review.empty?
   end
 end
-
-end
 puts hash
+
+# Now to push hash into database
+
+hash.keys.each do |name|
+  Review.create!(person: name, review: hash[name][:review], img_url: hash[name][:img])
+end
