4211997157b0e50e57eddbce9061e6c06986ec00
Voxoff <guybennettjones@hotmail.co.uk>
2018-08-28 12:33:43 +0100

    Add dotenv - Protect my secret data in .env file; added bootstrap; cloudinary

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.5'
+gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -23,15 +23,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
-# Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'bootstrap'
+gem 'cloudinary'
+gem 'dotenv-rails', groups: [:development, :test]
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+hash = {}
+
+(1..6).each do |element|
+  element = "" if element == 1
+  file = File.read("../../../lewagon/reviews/Kitt#{element}.html")
+  html_doc = Nokogiri::HTML(file)
+  html_doc.search('.student-content').each do |e|
+    img =  e.search('.img-md-square').to_a.first.attributes["src"].value
+    name = e.search('.feedback-content').children.first.text.strip
+    review = e.search('.feedback-content').children.search('.ml-1').text.strip
+    hash[name] = {img: img, review: review} unless review.empty?
+  end
+end
+# puts hash
+
+# Now to push hash into database
+
+hash.keys.each do |name|
+  Review.create!(person: name, review: hash[name][:review], img_url: hash[name][:img])
+end
