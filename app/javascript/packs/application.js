570f086ebd4f61043f7fda63c51793714fc4d330
Voxoff <guybennettjones@hotmail.co.uk>
2018-10-05 12:50:15 +0100

    fullpage, three and velocity experiments

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+import 'fullpage' from 'fullpage'; 
+new fullpage('#fullpage', {
+    navigation: true,
+    navigationPosition: 'left',
+    showActiveTooltip: true,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    autoScrolling:true,
+    autoScrolling:true
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'teacher', to: 'pages#teacher'
+  get 'play', to:'pages#play'
   resources :reviews, only: :index
 end
