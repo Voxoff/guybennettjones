980ff77103f29472e88607380ada0db15dc7dff8
Voxoff <guybennettjones@hotmail.co.uk>
2018-10-06 22:16:54 +0100

    the css has been halved

--- app/assets/javascripts/hex.js
+++ app/assets/javascripts/hex.js
@@ -1,27 +1,24 @@
+function hex_initial_animation() {
+  $(".hex-wrap").velocity("transition.slideLeftIn", { stagger: 100 });
+  $(".hex-wrap").velocity("callout.pulse");
+  $(".hoverblock").velocity("fadeOut", { delay: 2500, duration: 0 });
+}
+
+function hex_description(desc, title) {
+  $('.code-description').velocity('stop', true).velocity("transition.slideRightBigIn");
+  $('.' + desc).siblings().removeClass('desc-active');
+  $('.' + desc).addClass('desc-active');
+  $('.desc-active').children().velocity('stop', true).velocity("transition.slideRightBigIn", { stagger: 300 });
+  $('.code-title').text(title)
+}
+
 $(document).ready(function () {
-  function hex_initial_animation() {
-    $(".hex-wrap").velocity("transition.slideLeftIn", { stagger: 100 });
-    $(".hex-wrap").velocity("callout.pulse");
-    $(".hoverblock").velocity("fadeOut", { delay: 2500, duration: 0 });
-  }
   hex_initial_animation();
 
   $(".hex-init").mouseenter(function () {
-
-    var title_name = $(this).parent().attr("data-title");
-    var desc_name = $(this).parent().attr("data-content");
-
-    function hex_description() {
-      $('.code-description').velocity('stop', true).velocity("transition.slideRightBigIn");
-      $('.' + desc_name).siblings().removeClass('desc-active');
-      setTimeout(function() {
-        $('.' + desc_name).addClass('desc-active');
-        $('.desc-active').children().velocity('stop', true).velocity("transition.slideRightBigIn", { stagger: 300 });
-        $('.code-title, .desc-active span').velocity( { queue: false });
-        $('.code-title').text(title_name)
-      }, 0);
-    }
-    hex_description();
+    let title = $(this).parent().attr("data-title");
+    let desc = $(this).parent().attr("data-content");
+    hex_description(desc, title);
 
     $(this).parent().addClass('hexactive'); 
     $('.hexactive').velocity({scale: "1.1"}, { duration: 100 });
