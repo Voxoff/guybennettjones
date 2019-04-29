0e43088f83f6a080baa72c04592f6a411870094b
Guy Bennett-Jones <Voxoff>
2019-04-29 15:10:29 +0100

    edward at ze top pls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @top_reviews = Review.where(person: ['Izzy Brown','Angele Lenglemetz', 'Alisa Ming Yi Ng', 'Edward Ward'])
+    @top_reviews = Review.where(person: ['Edward Ward', 'Izzy Brown','Angele Lenglemetz', 'Alisa Ming Yi Ng'])
     @lw_reviews = @top_reviews + (Review.where(workplace: 'Le Wagon') - @top_reviews)
     @ff_reviews = Review.where(workplace: 'fourfivecbd')
     @fl_reviews = Review.where(workplace: 'Flatiron')
